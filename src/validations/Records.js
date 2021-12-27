69cc949dfdca91d8945b4133c8418c53dee70e0f
erdemcemozer <44067178+erdemcemozer@users.noreply.github.com>
2021-12-27 21:31:19 +0300

    Added date validations

--- src/validations/Records.js
+++ src/validations/Records.js
@@ -3,7 +3,11 @@ const Joi = require("joi").extend(require("@joi/date"));
 const validateFindRecords = Joi.object({
   // Validation for findRecords
   startDate: Joi.date().required().format("YYYY-MM-DD"),
-  endDate: Joi.date().required().format("YYYY-MM-DD"),
+  endDate: Joi.date()
+    .required()
+    .format("YYYY-MM-DD")
+    .less(new Date()) // To check if the endDate is less than today's date
+    .greater(Joi.ref("startDate")), // To check if the endDate is greater than startDate
   minCount: Joi.number().required().min(0),
   maxCount: Joi.number().required().min(0),
 });
